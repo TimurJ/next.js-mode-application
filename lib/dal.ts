9e3cfe00be90bafc8c1028540263dae8ba41bac2
Timur Jalilov <timurjalilov1@gmail.com>
2025-09-30 16:19:38 +0100

    remove unused code and fix errors

--- lib/dal.ts
+++ lib/dal.ts
@@ -2,7 +2,7 @@ import { db } from '@/db'
 import { getSession } from './auth'
 import { eq } from 'drizzle-orm'
 import { cache } from 'react'
-import { issues, users } from '@/db/schema'
+import { users } from '@/db/schema'
 import { mockDelay } from './utils'
 
 //Current user
